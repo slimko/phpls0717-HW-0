565d1dfe65e067fe3e283482fead620048a559e8
denisden83 <denisden83@gmail.com>
2017-07-12 22:36:09 +0200

    #2.master launched 'awasome project'

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-
-13
-11
-77 99 23
-77 15 33 67
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AP</title>
+</head>
+<body>
+    <h1 class="title">awesome project</h1>
+</body>
+</html>
 
