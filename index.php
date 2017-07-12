394092cf6e7f87f6af27bb3a714d2b89aa77f48e
denisden83 <denisden83@gmail.com>
2017-07-12 23:34:47 +0200

    #4.master changed html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,18 +7,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AP</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <h1 class="title">awesome project</h1>
 </body>
 </html>
 
-=======
-12
-14
-76 98 24
-72 18 34 68
-4444266
-88888
-10 30 2
->>>>>>> 8919558cdb814d1a3035cc4737c2475243b8a841
+
